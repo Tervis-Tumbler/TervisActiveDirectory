f6db4c67626f6f986d22fa0e06563d2fdd3d500b
Chris Magnuson <hourglasssand@gmail.com>
2017-01-24 10:46:45 -0500

    Add operational framework validation

---
+++ Diagnostics/Comprehensive/Comprehensive.Tests.ps1
@@ -0,0 +1,50 @@
+﻿$ADUsers = Get-ADUser -Filter * -SearchBase "OU=Departments,DC=tervis,DC=prv" -Properties HomeDirectory, Manager, EmployeeID, MemberOf, PasswordNeverExpires, Department, TelephoneNumber
+
+foreach ($ADUser in $ADUsers) {
+    Describe "Active Directory User" {
+        It "$($ADUser.Name) ($($ADUser.samaccountname)) should have an employee ID" {
+            
+            $ADUserThatShouldHaveEmployeeID = $ADUSer | 
+            where DistinguishedName -NotMatch "OU=Store Accounts,OU=Users,OU=Stores,OU=Departments" |
+            where {-not ($_.MemberOf -Match "CN=Contractor,")} |
+            where {-not ($_.MemberOf -Match "CN=SharedAccountsThatNeedToBeAddressed,")} |
+            where {-not ($_.MemberOf -Match "CN=Test Users,")}
+            
+            if ($ADUserThatShouldHaveEmployeeID) {
+                $ADUserThatShouldHaveEmployeeID.EmployeeID | Should Not BeNullOrEmpty
+            }
+        }
+
+        It "$($ADUser.Name) ($($ADUser.samaccountname)) should have a manager set" {
+            $ADUserThatShouldHaveManager = $ADUSer | 
+            where DistinguishedName -NotMatch "OU=Store Accounts,OU=Users,OU=Stores,OU=Departments" |
+            where {-not ($_.MemberOf -Match "CN=Contractor,")} |
+            where {-not ($_.MemberOf -Match "CN=SharedAccountsThatNeedToBeAddressed,")} |
+            where {-not ($_.MemberOf -Match "CN=Test Users,")}
+            
+            if ($ADUserThatShouldHaveManager) {
+                $ADUserThatShouldHaveManager.Manager | Should Not BeNullOrEmpty
+            }
+        }
+ 
+        It "If $($ADUser.Name) ($($ADUser.samaccountname)) has a home directory, it should exist" {
+            if ($ADUSer.HomeDirectory) {
+                $ADUSer.HomeDirectory | should exist
+            }
+        }
+
+        It "$($ADUser.Name) ($($ADUser.samaccountname)) should not have the PasswordNeverExpires enabled" {
+            $ADUserThatShouldNotHavePasswordNeverExpires = $ADUSer |
+            where DistinguishedName -NotMatch "OU=Store Accounts,OU=Users,OU=Stores,OU=Departments" |
+            where {-not ($_.MemberOf -Match "CN=SharedAccountsThatNeedToBeAddressed,")}
+
+            if ($ADUserThatShouldNotHavePasswordNeverExpires) {
+                $ADUserThatShouldNotHavePasswordNeverExpires.PasswordNeverExpires | Should Be $false
+            }
+        }
+
+        It "$($ADUser.Name) ($($ADUser.samaccountname)) should have a department set" {
+            $ADUser.Department | Should Not BeNullOrEmpty
+        }
+    }
+}
---
+++ Diagnostics/Simple/Simple.Tests.ps1
@@ -0,0 +1,8 @@
+﻿Describe "Active Directory Users In Departments OU" {
+    $ADUsers = Search-ADAccount -PasswordNeverExpires -SearchBase "OU=Departments,DC=tervis,DC=prv" |
+
+    It "Shouldn't have PasswordSetToNeverExpire set to true" {
+        Search-ADAccount -PasswordNeverExpires -SearchBase "OU=Departments,DC=tervis,DC=prv" |
+        Should BeNullOrEmpty
+    }
+}
