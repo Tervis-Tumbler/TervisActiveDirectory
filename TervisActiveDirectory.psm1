10bbae3d934eabff6b099999ead8b173dbd78e6e
ChrisMagnuson <hourglasssand@gmail.com>
2018-05-18 14:49:46 -0400

    Correct mispelling

--- TervisActiveDirectory.psm1
+++ TervisActiveDirectory.psm1
@@ -676,7 +676,7 @@ function Install-SendTervisInactivityNotification {
     }
 }
 
-functin Invoke-TervisActiveDirectoryCleanup {    
+function Invoke-TervisActiveDirectoryCleanup {    
     Disable-TervisADUserInactive
     Disable-TervisADComputerInactive
     Remove-TervisADUserInactive
